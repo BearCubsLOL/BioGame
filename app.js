5a01bea2a5964bd4412354fe49f87eab5f7bbb3b
Jay <151044640+BearCubsLOL@users.noreply.github.com>
2024-06-13 11:12:33 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const MainCharacter = docuemnt.querySelector('#mc');
+
+function characterController() {
+    document.addEventListener("keydown", (e) -> {
+       if (key.event == 87) {
+           console.log("HELLO WORLD")
+       } 
+    });
+}
+
+characterController()
